bb934258963c44e94aa197be082f480422f598d3
gennie-kr <allflare.kr@gmail.com>
2024-10-02 10:57:27 +0900

    django course 23 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 import os, environ
 
+from django.conf.global_settings import LOGIN_REDIRECT_URL
+from django.urls import reverse_lazy
 
 env = environ.Env(
     # set casting, default value
@@ -55,6 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'accountapp',
 
 ]
@@ -137,10 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -150,3 +150,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
