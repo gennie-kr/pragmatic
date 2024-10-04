5b35ad8cc317386c784601c007b58e7ef0931184
gennie-kr <allflare.kr@gmail.com>
2024-10-04 21:51:18 +0900

    django course 39 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -45,7 +45,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# 모두 허용
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
